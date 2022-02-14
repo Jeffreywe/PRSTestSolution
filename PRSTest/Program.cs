86d2e00f0236916ddd93a88ce29a47f109b326bb
Jeffrey Evans <jeffreyevans.qcec@aol.com>
2022-02-14 16:15:46 -0500

    Starting to work on the database now with EF

--- PRSTest/Program.cs
+++ PRSTest/Program.cs
@@ -1,4 +1,5 @@
-﻿using PRSLibrary.Models;
+﻿using PRSLibrary.Controllers;
+using PRSLibrary.Models;
 using System;
 using System.Linq;
 
@@ -6,9 +7,38 @@ namespace TestPRSLibrary {
     class Program {
         static void Main(string[] args) {
 
-            PRSDbContext context = new PRSDbContext();
-            
+            var context = new PRSDbContext(); // creates context variable from PRSDbContext
 
+            var userCtrl = new UsersController(context); //takes our context from userscontroller class and puts it into userctrl
+
+            //creates a new user
+/*            var newUser = new User() {
+                Id = 0, Username = "xx", Password = "xx",
+                Firstname = "User", Lastname = "XX",
+                Phone = "211", Email = "xx@user.com",
+                IsReviewer = false, IsAdmin = false
+            };
+            userCtrl.Create(newUser); // calls our Create method in our controller
+*/
+            //reads users in the list
+            var user3 = userCtrl.GetByPk(3);
+            if(user3 is null) {
+                Console.WriteLine("User not found!");
+            } else {
+                Console.WriteLine($"User3: {user3.Firstname} {user3.Lastname}");
+            }
+            var user33 = userCtrl.GetByPk(33);
+            if (user33 is null) {
+                Console.WriteLine("User not found!");
+            } else {
+                Console.WriteLine($"User33: {user33.Firstname} {user33.Lastname}");
+            }
+
+            var users = userCtrl.GetAll(); // calls the GetAll method in our controller
+            
+            foreach(var user in users) {
+                Console.WriteLine($"{user.Firstname} {user.Lastname}" );
+            }
         }
     }
 }
---
+++ PRSTestLibraries/Controllers/UsersController.cs
@@ -0,0 +1,54 @@
+﻿using PRSLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRSLibrary.Controllers {
+    /// <summary>
+    /// controller is a class that contains methods
+    /// 5 tables, 5 controllers
+    /// controllers will be in our capstone project, any client that talks to our database wil ltalk to our controller
+    /// controller allows us to add business rules, 
+    /// user changes item values, controller keeps the total accurate for the user
+    /// use another models folder called controllers for controllers to keep it organized
+    /// when you create controllers, you name the database and then the name controller
+    /// contexts in the controllers need to be private
+    /// toread,
+    /// insert,
+    /// update,
+    /// delete,
+    /// minimal methods built inside controllers
+    /// </summary>
+    public class UsersController {
+
+        private readonly PRSDbContext _context; // readonly means the only thing that can change the value of context is inside the constructor
+
+        public UsersController(PRSDbContext context) {
+            this._context = context; // takes this variable and puts it into _context
+        }
+
+        // the 2 read methods
+        public IEnumerable<User> GetAll() {// means we can pass a list or an array, general interface method, means we can take things and return what we want, flexibility
+            return _context.Users.ToList(); // converts to list
+        }
+        public User GetByPk(int id) {
+            return _context.Users.Find(id);
+        }
+
+        // maintenance functions
+        //insert
+        public User Create(User user) {
+            if(user is null) {
+                throw new ArgumentNullException("user");
+            }
+            if(user.Id != 0) {
+                throw new ArgumentException("User.Id must be zero!");
+            }
+            _context.Users.Add(user);
+            _context.SaveChanges();
+            return user;
+        }
+    }
+}
