be05640e3a2478f3ac2baf6d0394af5c830de0a2
Jeffrey Evans <jeffreyevans.qcec@aol.com>
2022-02-15 10:45:21 -0500

    added product controller

--- PRSTest/Program.cs
+++ PRSTest/Program.cs
@@ -5,50 +5,83 @@ using System.Linq;
 
 namespace TestPRSLibrary {
     class Program {
+
+        static void Print(Product product) { // useful technique, create methods to do the work for you instead of having to write the code out over and over
+            Console.WriteLine($"{product.Id,-5} {product.PartNbr,-12} {product.Name,-12} {product.Price,10:c} {product.Vendor.Name,-15}"); 
+        }
+
         static void Main(string[] args) {
 
             var context = new PRSDbContext(); // creates context variable from PRSDbContext
 
-            var userCtrl = new UsersController(context); //takes our context from userscontroller class and puts it into userctrl
+            var prodCtrl = new ProductsController(context);
 
-            //creates a new user
-/*            var newUser = new User() {
-                Id = 0, Username = "xx", Password = "xx",
-                Firstname = "User", Lastname = "XX",
-                Phone = "211", Email = "xx@user.com",
-                IsReviewer = false, IsAdmin = false
-            };
-            userCtrl.Create(newUser); // calls our Create method in our controller
-*/
+            var products = prodCtrl.GetAll();
 
-            //reads users in the list
-            var user3 = userCtrl.GetByPk(3);
-            if(user3 is null) {
-                Console.WriteLine("User not found!");
-            } else {
-                Console.WriteLine($"User3: {user3.Firstname} {user3.Lastname}");
-            }
-            //updates a user
-            //user3.Lastname = "User3";
-            //userCtrl.Change(user3);
-
-
-            //reads users in lists
-            var user33 = userCtrl.GetByPk(33);
-            if (user33 is null) {
-                Console.WriteLine("User not found!");
-            } else {
-                Console.WriteLine($"User33: {user33.Firstname} {user33.Lastname}");
+            foreach(var p in products) {
+                Print(p);
             }
 
-            //removes a user, Id has to be valid
-            userCtrl.Remove(4);
+            var product = prodCtrl.GetByPk(2);
 
-            var users = userCtrl.GetAll(); // calls the GetAll method in our controller
-            
-            foreach(var user in users) {
-                Console.WriteLine($"{user.Id} {user.Firstname} {user.Lastname}" );
+            if(product is not null) {
+                Print(product); // takes the variable and passes it into our Print method
             }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            //USERS
+            //            var userCtrl = new UsersController(context); //takes our context from userscontroller class and puts it into userctrl
+
+            //            //creates a new user
+            ///*            var newUser = new User() {
+            //                Id = 0, Username = "xx", Password = "xx",
+            //                Firstname = "User", Lastname = "XX",
+            //                Phone = "211", Email = "xx@user.com",
+            //                IsReviewer = false, IsAdmin = false
+            //            };
+            //            userCtrl.Create(newUser); // calls our Create method in our controller
+            //*/
+
+            //            //reads users in the list
+            //            var user3 = userCtrl.GetByPk(3);
+            //            if(user3 is null) {
+            //                Console.WriteLine("User not found!");
+            //            } else {
+            //                Console.WriteLine($"User3: {user3.Firstname} {user3.Lastname}");
+            //            }
+            //            //updates a user
+            //            //user3.Lastname = "User3";
+            //            //userCtrl.Change(user3);
+
+
+            //            //reads users in lists
+            //            var user33 = userCtrl.GetByPk(33);
+            //            if (user33 is null) {
+            //                Console.WriteLine("User not found!");
+            //            } else {
+            //                Console.WriteLine($"User33: {user33.Firstname} {user33.Lastname}");
+            //            }
+
+            //            //removes a user, Id has to be valid
+            //            userCtrl.Remove(4);
+
+            //            var users = userCtrl.GetAll(); // calls the GetAll method in our controller
+
+            //            foreach(var user in users) {
+            //                Console.WriteLine($"{user.Id} {user.Firstname} {user.Lastname}" );
+            //            }
         }
     }
 }
--- PRSTestLibraries/Controllers/ProductsController.cs
+++ PRSTestLibraries/Controllers/ProductsController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using PRSLibrary.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,5 +9,37 @@ using System.Threading.Tasks;
 namespace PRSLibrary.Controllers {
     public class ProductsController {
 
+        private readonly PRSDbContext _context;
+
+        public ProductsController(PRSDbContext context) {
+            _context = context;
+        }
+        
+        public IEnumerable<Product> GetAll() { // this change only applies for the read operations to pull vendor name from vendors class
+            return _context.Products.Include(x => x.Vendor).ToList(); // Vendor from the instance that creates the FK in product class
+        }
+
+        public Product GetByPk(int id) {
+            return _context.Products.Include(x => x.Vendor)
+                                .SingleOrDefault(x => x.Id == id); // another way to get the same result as .Find, but useable with .Include
+        }
+
+        public Product Create(Product product) {
+            _context.Products.Add(product);
+            _context.SaveChanges();
+            return product; 
+        }
+
+        public void Change(Product product) {
+            _context.SaveChanges();
+        }
+
+        public void Remove(int id) {
+            var product = _context.Products.Find(id);
+            if(product is not null) {
+                _context.Products.Remove(product);
+                _context.SaveChanges();
+            }
+        }
     }
 }
