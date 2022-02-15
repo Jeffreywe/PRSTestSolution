5014085402a154fbe067f9a4fd9094eea3ef389e
Jeffrey Evans <jeffreyevans.qcec@aol.com>
2022-02-15 16:03:59 -0500

    added code to explain what the RecalculateRequestTotal method does

--- PRSTestLibraries/Controllers/RequestlinesController.cs
+++ PRSTestLibraries/Controllers/RequestlinesController.cs
@@ -14,7 +14,15 @@ namespace PRSLibrary.Controllers {
         public RequestlinesController(PRSDbContext context) {
             _context = context;
         }
-
+        /// <summary>
+        /// reads the request by the requestid we insert in the parameter and finding it using .find, and then we create the syntax to put into the request.Total var
+        /// from r1 is fred, in _con.Rlines is returning all lines, then we are joining to products since PK and FK on clause joins together
+        /// where sets condition that brings back rows where requests thats passed matches the requestid put into parameter of calculate method
+        /// select returns some of the columns not all of them by identifying with new statement, take rl.Quatity and p.Price and returns inside new column named linetotal
+        /// wrapping that in parans lets us use the .Sum method to sum up the line totals we mathed out in the query syntax
+        /// the sum gets placed into the request total in the beginning with =, and then save changes saves to the database
+        /// </summary>
+        /// <param name="requestId"></param>
         private void RecalculateRequestTotal(int requestId) { // this recalculates the products and requests amount and stores it into the one request for the userids related
             var request = _context.Requests.Find(requestId); // finds the requestid and gives the data realted to it
 
